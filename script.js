e731d0a3e1012a12a736332f56fd4aea1b51f89d
ShahbazAli2001 <shahbazali5097855@gmail.com>
2024-12-01 13:55:31 +0500

    Project Finalized

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random()*20)+1;
+console.log(secretNumber);
 let score =20;
 let highscore =0;
+
+
+
 
 
 //Function to Display Message
@@ -10,15 +14,52 @@ const displayMessage = function(message){
     document.querySelector('.message').textContent = message;
 }
 
-//Function to Fecth Value of the Input and Then Convert to Number
-document.querySelector(".check").addEventListener("click", function(){
-    const guess = Number(document.querySelector(".guess").value);
-})
-
+//Function to Fetch Value of the Input and Then Convert to Number
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    
 if  (!guess){
+
     displayMessage("⛔️ No number!");
+
 }else if (guess === secretNumber) {
     displayMessage("👏 🎉 Correct Number!");
-    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+    }
+
+
+}else if(guess !== secretNumber){
+    if(score > 1){
+        displayMessage(guess > secretNumber ? " 😮 Number Too High" : " 😣 Number Too Low");
+        score--;
+        document.querySelector('.score').textContent = score;
+    }else {
+        displayMessage("🤦‍♂️😒 You've lost the game! ");
+        document.querySelector('.score').textContent = 0;
+    }
+
+}
 }
 
+);
+// Playing Again
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+  
+    // document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+  
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+  });
